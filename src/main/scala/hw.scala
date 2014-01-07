efc396858c9a2008fdf292f5d423e9d3333fdfcc
Artem Nikiforov <lnxmnx@gmail.com>
2014-01-07 12:00:56 +0400

    This one can be used as "sbt" and then "run" multiple times.

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -3,7 +3,7 @@ import org.opencv.core.CvType;
 
 object Hi {
   def main(args: Array[String]) = {
-    System.loadLibrary("opencv_java247")
+    System.load("/home/username/sbt-run/lib/libopencv_java247.so")
     val m  = Mat.eye(3, 3, CvType.CV_8UC1)
     println("m = " + m.dump())
   }
